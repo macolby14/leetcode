83ab8fa12111cce14a57d03a83fc44a2b0ed656e
Mark Colby <macolby14@gmail.com>
2021-05-21 07:40:50 -1000

    solve find and replace pattern medium

---
+++ may21/find-and-replace-pattern.ts
@@ -0,0 +1,26 @@
+function findAndReplacePattern(words: string[], pattern: string): string[] {
+    const matches: string[]=[];
+    for(const word of words){
+        if(isPatternMatch(word,pattern)){matches.push(word);}
+    }
+    return matches;
+};
+
+function isPatternMatch(s: string, p: string): boolean{
+    if(s.length!==p.length){return false;}
+    
+    const map = new Map<string, string>(); //pattern to string character
+    const seen = new Set<string>();
+    
+    for(let i=0;i<s.length;i++){
+        const sC=s[i];
+        const pC=p[i];
+        if(map.has(pC)||seen.has(sC)){
+            if(map.get(pC)!==sC){return false;}
+        }else{
+            map.set(pC,sC);
+            seen.add(sC);
+        }
+    }
+    return true;
+}
